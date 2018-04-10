fbeb1bd011419b12136e5710d609d0cf5b480c59
Oliver Stenbom <ostenbom@pivotal.io>
2018-04-10 10:25:55 +0100

    Refactor tests to mock out Redis entirely
    Two instances of Redis we're required to run tests. Removed and mocked
    this dependency as we can trust @Cacheable to be tested elsewhere and
    what we really need to test is our gluing code
    
    [#156304008]
    
    Signed-off-by: Maya Rosecrance <mrosecrance@pivotal.io>

--- examples/multiple-redis-high-availability/src/test/java/io/pivotal/pcfredis/multiredis/MultipleRedisHighAvailabilityApplicationIT.java
+++ examples/multiple-redis-high-availability/src/test/java/io/pivotal/pcfredis/multiredis/MultipleRedisHighAvailabilityApplicationIT.java
@@ -17,14 +17,15 @@ import static java.util.UUID.randomUUID;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MultipleRedisHighAvailabilityApplicationIT {
+
+    private String tokenID = "123";
+    private Token token = new Token(tokenID, "token-data");
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -37,23 +38,36 @@ public class MultipleRedisHighAvailabilityApplicationIT {
     @SpyBean
     private CacheManager secondaryCacheManager;
 
-    @Test public void
-    should_create_a_token_in_the_redis_when_it_does_not_exist() {
+
+    @Test
+    public void
+    should_look_in_the_first_redis_for_tokens() {
+        mockCache(primaryCacheManager, new tokenValueWrapper());
+
+        Token finalToken = retrieveToken(tokenID);
+        assertThat(token, is(finalToken));
+    }
+
+    @Test
+    public void
+    should_look_in_the_second_redis_when_the_first_does_not_have_it() {
+        mockCache(primaryCacheManager, null);
+        mockCache(secondaryCacheManager, new tokenValueWrapper());
+
+        Token finalToken = retrieveToken(tokenID);
+
+        assertThat(token, is(finalToken));
+    }
+
+    @Test
+    public void
+    should_create_a_token_when_token_is_not_in_either_cache() {
         String tokenId = "new-" + randomUUID().toString();
+        mockCache(primaryCacheManager, null);
+        mockCache(secondaryCacheManager, null);
         ResponseEntity<Token> myToken = this.restTemplate.getForEntity("/token?id={t}", Token.class, tokenId);
 
         assertThat(myToken.getBody().getId(), is(tokenId));
-    }
-
-    @Test public void
-    should_look_in_the_second_redis_when_the_first_is_down() {
-        String tokenId = "when-1st-redis-is-down-" + randomUUID();
-        Token initialToken = saveTokenInBothCaches(tokenId);
-        turnOffFirstCache();
-
-        Token finalToken = retrieveToken(tokenId);
-
-        assertThat(initialToken, is(finalToken));
     }
 
     private Token retrieveToken(String tokenId) {
@@ -61,22 +75,19 @@ public class MultipleRedisHighAvailabilityApplicationIT {
         return response.getBody();
     }
 
-    private void turnOffFirstCache() {
+    private Cache mockCache(CacheManager manager, tokenValueWrapper tokenVW) {
         Cache cache = mock(Cache.class);
-        when(cache.get(any())).thenReturn(null);
-        doReturn(cache).when(primaryCacheManager).getCache("tokens");
+        when(cache.get(any())).thenReturn(tokenVW);
+        doReturn(cache).when(manager).getCache("tokens");
+        return cache;
     }
 
-    private Token saveTokenInBothCaches(String tokenId) {
-        Token token = this.restTemplate.getForEntity("/token?id={t}", Token.class, tokenId).getBody();
-        checkExistsInCache(token, primaryCacheManager);
-        checkExistsInCache(token, secondaryCacheManager);
-        return token;
-    }
+    class tokenValueWrapper implements Cache.ValueWrapper {
 
-    private static void checkExistsInCache(Token token, CacheManager cacheManager) {
-        Cache.ValueWrapper tokens = cacheManager.getCache("tokens").get(token.getId());
-        assertThat(tokens.get(), is(token));
+        @Override
+        public Object get() {
+            return token;
+        }
     }
 
 
