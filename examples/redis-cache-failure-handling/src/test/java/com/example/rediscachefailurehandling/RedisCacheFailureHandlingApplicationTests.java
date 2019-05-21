0e8f562b1734b541c43a88b1f247dcb7d8b2f60a
Grzegorz Tatarzyn <gtatarzyn@pivotal.io>
2019-05-21 16:00:11 +0100

    Refactor tests.
    [#163787573]
    
    Signed-off-by: Mirah Gary <mgary@pivotal.io>

--- examples/redis-cache-failure-handling/src/test/java/com/example/rediscachefailurehandling/RedisCacheFailureHandlingApplicationTests.java
+++ examples/redis-cache-failure-handling/src/test/java/com/example/rediscachefailurehandling/RedisCacheFailureHandlingApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.rediscachefailurehandling;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
@@ -13,7 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.beans.SamePropertyValuesAs.samePropertyValuesAs;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
@@ -29,37 +30,35 @@ public class RedisCacheFailureHandlingApplicationTests {
 	@SpyBean
 	private CacheManager manager;
 
+	@Mock
+	public Cache cache;
+
+	@Before
+	public void setup() {
+		doReturn(cache).when(manager).getCache("tokens");
+	}
+
 	@Test
-	public void shouldCreateNewTokenWhenItDoesNotExistOnCache() {
-		mockCache(manager, null);
+	public void shouldCreateNewTokenWhenItDoesNotExistInCache() {
+		when(cache.get(tokenId)).thenReturn(() -> null);
+
 		Token finalToken = retrieveToken(tokenId);
 
 		assertThat(finalToken, notNullValue());
+		verify(cache).get(tokenId);
 	}
 
 	@Test
 	public void shouldGetTokenFromCache() {
-		mockCache(manager, new TokenValueWrapper());
+		when(cache.get(tokenId)).thenReturn(() -> token);
+
 		Token finalToken = retrieveToken(tokenId);
+
 		assertThat(finalToken, samePropertyValuesAs(token));
+		verify(cache).get(tokenId);
 	}
 
 	private Token retrieveToken(String tokenId) {
 		return this.restTemplate.getForObject("/token?id={t}", Token.class, tokenId);
-	}
-
-	private Cache mockCache(CacheManager manager, TokenValueWrapper tokenVW) {
-		Cache cache = mock(Cache.class);
-		when(cache.get(any())).thenReturn(tokenVW);
-		doReturn(cache).when(manager).getCache("tokens");
-		return cache;
-	}
-
-	class TokenValueWrapper implements Cache.ValueWrapper {
-
-		@Override
-		public Object get() {
-			return token;
-		}
 	}
 }
